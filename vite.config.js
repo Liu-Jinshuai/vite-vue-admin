3dbad153444267a55c772d813b1280551ff048c6
li.jinshuai <liujinshuai521@163.com>
2024-01-04 14:50:44 +0800

    up base

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import mockData from 'vite-plugin-mock-data';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/vite-vue-admin/',
   plugins: [
     vue(),
     mockData({ mockRoutesDir: './mock' })
